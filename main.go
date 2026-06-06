bec88c9c3f5f35aee19708956c3e3b2554db446b
npikall <nikolas.pikall@gmail.com>
2026-06-06 03:50:49 +0200

    refactor: clean up code

--- main.go
+++ main.go
@@ -14,67 +14,46 @@ import (
 	"github.com/nickng/bibtex"
 )
 
-var logger = log.Default()
-
-type customClient struct {
+type Client struct {
 	client *http.Client
 	email  string
+}
+
+type Result struct {
+	status     string
+	statusCode int
 }
 
 func main() {
 	email := flag.String("email", "", "An email to get better rate limits from Crossref API")
 	flag.Parse()
 
-	file := "refs.bib"
-	if len(os.Args) > 1 {
-		file = os.Args[1]
-	}
+	file := getFilePath()
 	bib := parseBibFile(file)
 
-	client := &customClient{
-		client: &http.Client{Timeout: 5 * time.Second},
-		email:  *email,
-	}
-
-	numWorkers := min(runtime.NumCPU(), 8)
-	numJobs := len(bib.Entries)
-	doiCh := make(chan string, numJobs)
-	resCh := make(chan string, numJobs)
-	var wg sync.WaitGroup
-	for range numWorkers {
-		wg.Go(func() {
-			worker(client, doiCh, resCh)
-		})
-	}
-	go func() {
-		wg.Wait()
-		close(resCh)
-	}()
-
-	for _, entry := range bib.Entries {
-		doiField, found := entry.Fields["doi"]
-		if !found {
-			logger.Println("[WARN] no doi found:", entry.CiteName)
-			continue
-		}
-
-		doi := normalizeDOI(doiField.String())
-		doiCh <- doi
-	}
-	close(doiCh)
+	client := NewClient(*email)
+	resCh := processData(client, bib.Entries)
 
 	for result := range resCh {
-		logger.Println(result)
+		fmt.Println(result)
 	}
 }
 
-func worker(client *customClient, doiCh <-chan string, resCh chan<- string) {
+func getFilePath() string {
+	file := "refs.bib"
+	if flag.NArg() > 1 {
+		file = flag.Arg(0)
+	}
+	return file
+}
+
+func worker(client *Client, doiCh <-chan string, resCh chan<- string) {
 	for doi := range doiCh {
 		resCh <- checkDOIWithRetry(client, doi)
 	}
 }
 
-func checkDOIWithRetry(client *customClient, doi string) string {
+func checkDOIWithRetry(client *Client, doi string) string {
 	backoff := 500 * time.Millisecond
 	for {
 		res := checkDOI(client, doi)
@@ -90,7 +69,7 @@ func checkDOIWithRetry(client *customClient, doi string) string {
 	}
 }
 
-func checkDOI(c *customClient, doi string) *result {
+func checkDOI(c *Client, doi string) *Result {
 	req, err := http.NewRequest(http.MethodHead, "https://api.crossref.org/works/doi"+doi, nil)
 	if err != nil {
 		return nil
@@ -104,23 +83,25 @@ func checkDOI(c *customClient, doi string) *result {
 	}
 	defer resp.Body.Close()
 
-	return &result{status: resp.Status, statusCode: resp.StatusCode}
+	return &Result{status: resp.Status, statusCode: resp.StatusCode}
 }
 
-type result struct {
-	status     string
-	statusCode int
+func NewClient(email string) *Client {
+	return &Client{
+		client: &http.Client{Timeout: 5 * time.Second},
+		email:  email,
+	}
 }
 
 func parseBibFile(file string) *bibtex.BibTex {
 	f, err := os.Open(file)
 	if err != nil {
-		logger.Fatal(err)
+		log.Fatal(err)
 	}
 
 	bib, err := bibtex.Parse(f)
 	if err != nil {
-		logger.Fatal(err)
+		log.Fatal(err)
 	}
 	return bib
 }
@@ -130,4 +111,42 @@ func normalizeDOI(doi string) string {
 	doi = strings.TrimPrefix(doi, "http://doi.org/")
 	doi = strings.TrimPrefix(doi, "https://doi.org/")
 	return doi
+}
+
+func processData(client *Client, entries []*bibtex.BibEntry) chan string {
+	numWorkers := min(runtime.NumCPU()-1, 8)
+	numJobs := len(entries)
+
+	jobCh := make(chan string, numJobs)
+	resCh := make(chan string, numJobs)
+
+	// Start up worker goroutines
+	var wg sync.WaitGroup
+	for range numWorkers {
+		wg.Go(func() {
+			worker(client, jobCh, resCh)
+		})
+	}
+	// defer closing result channel
+	go func() {
+		wg.Wait()
+		close(resCh)
+	}()
+
+	// send jobs to the workers
+	for _, entry := range entries {
+		processEntry(jobCh, entry)
+	}
+	close(jobCh)
+	return resCh
+}
+
+func processEntry(jobCh chan<- string, entry *bibtex.BibEntry) {
+	doiField, found := entry.Fields["doi"]
+	if !found {
+		fmt.Println("[WARN] no doi found:", entry.CiteName)
+		return
+	}
+	doi := normalizeDOI(doiField.String())
+	jobCh <- doi
 }
